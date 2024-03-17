9a50647ee93dff16ae53edede2b7e4049c48f80f
SurjeetMandal <surjeetmandal698@gmail.com>
2024-03-17 11:27:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 const makeChange = (c) => {
-  // your name here
+	const obj = [
+		{value: 25, dollar: q},
+		{value: 10, dollar: d},
+		{value: 5, dollar: n},
+		{value: 1, dollar: p}	
+	];
+	let change = {
+		q: 0,
+		d: 0,
+		n: 0,
+		p: 0
+	}
+	for (let i = 0; i < obj.length; i++) {
+		while (c >= obj[i].value) {
+			change[obj[i].dollar]++;
+			c -= obj[i].value;
+		}
+	}
+	return change;
 };
 
 // Do not the change the code below
-const c = prompt("Enter c: ");
-alert(JSON.stringify(makeChange(c)));
+// const c = prompt("Enter c: ");
+// alert(JSON.stringify(makeChange(c)));
+
